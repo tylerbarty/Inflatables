92afe5dd547349b5551fc1e7a927eb1101777908
Tyler Barty <tyler.barty@gmail.com>
2021-04-06 12:13:33 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inflatables.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
